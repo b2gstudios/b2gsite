b6847ff11a215e5fff066573f9fc8f7abdccacd9
Tom Gabrysiak <tomgabrysiak@gmail.com>
2012-05-27 20:57:25 -0400

    initial commit

---
+++ comments/connect.php
@@ -0,0 +1,18 @@
+<?php
+
+/* Database config */
+
+$db_host		= 'localhost';
+$db_user		= 'root';
+$db_pass		= 'root';
+$db_database		= 'comments'; 
+
+/* End config */
+
+
+$link = @mysql_connect($db_host,$db_user,$db_pass) or die('Unable to establish a DB connection');
+
+mysql_query("SET NAMES 'utf8'");
+mysql_select_db($db_database,$link);
+
+?>
